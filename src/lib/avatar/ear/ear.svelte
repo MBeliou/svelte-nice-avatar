43628e3197b2a1ffa078dac8497e4a88d02e3362
Maxime Bele <maxime.bele@gmail.com>
2024-12-30 11:32:58 +0100

    port utils, ears and eyebrows

---
+++ src/lib/avatar/ear/ear.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { EarSize } from '$lib/avatar/types.js';
+	import Big from './big.svelte';
+	import Small from './small.svelte';
+
+	let { size, color }: { size: EarSize; color: string } = $props();
+</script>
+
+{#if size === 'small'}
+	<Small {color} />
+{:else}
+	<Big {color} />
+{/if}
---
+++ src/lib/avatar/eyebrow/eyebrow.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { EyeBrowStyle } from '../types.js';
+	import Up from './up.svelte';
+	import UpWoman from './upWoman.svelte';
+
+	let { style }: { style: EyeBrowStyle } = $props();
+</script>
+
+{#if style === 'up'}
+	<Up></Up>
+{:else}
+	<UpWoman></UpWoman>
+{/if}
