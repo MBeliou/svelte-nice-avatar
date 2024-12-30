a4a0bd663d9340a91dccd4eb41ec6273c76e2ddf
Maxime Bele <maxime.bele@gmail.com>
2024-12-30 11:59:45 +0100

    port eyes

---
+++ src/lib/avatar/eyes/eyes.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { EyeStyle } from '../types.js';
+	import Circle from './circle.svelte';
+	import Oval from './oval.svelte';
+	import Smile from './smile.svelte';
+
+	let { style }: { style: EyeStyle } = $props();
+</script>
+
+{#if style === 'circle'}
+	<Circle></Circle>
+{:else if style === 'oval'}
+	<Oval></Oval>
+{:else}
+	<Smile></Smile>
+{/if}
