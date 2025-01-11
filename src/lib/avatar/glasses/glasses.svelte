71f847ca3763aed15199308732b16f9abc53717c
Maxime Bele <maxime.bele@gmail.com>
2025-01-11 20:36:54 +0100

    add glasses

---
+++ src/lib/avatar/glasses/glasses.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Round from './round.svelte';
+	import Square from './square.svelte';
+
+	let { style }: { style: 'round' | 'square' | 'none' } = $props();
+</script>
+
+{#if style === 'round'}
+	<Round></Round>
+{:else if style === 'square'}
+	<Square></Square>
+{/if}
