53e367a8f43d7fb062fd2349d8d5187edca6a57b
Maxime Bele <maxime.bele@gmail.com>
2025-01-11 20:52:58 +0100

    add hair

---
+++ src/lib/avatar/hair/hair.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Normal from './normal.svelte';
+	import Thick from './thick.svelte';
+	import Mohawk from './mohawk.svelte';
+	import WomanLong from './womanLong.svelte';
+	import WomanShort from './womanShort.svelte';
+
+	let {
+		style,
+		color,
+		colorRandom
+	}: {
+		style: 'thick' | 'mohawk' | 'womanLong' | 'womanShort' | 'normal';
+		color: string;
+		colorRandom: boolean;
+	} = $props();
+
+	let Component = (() => {
+		switch (style) {
+			case 'thick':
+				return <Thick color={color} colorRandom={colorRandom} />;
+			case 'mohawk':
+				return <Mohawk color={color} colorRandom={colorRandom} />;
+			case 'womanLong':
+				return <WomanLong color={color} />;
+			case 'womanShort':
+				return <WomanShort color={color} />;
+			case 'normal':
+			default:
+				return <Normal color={color} />;
+		}
+	})();
+</script>
+
+<Component></Component>
