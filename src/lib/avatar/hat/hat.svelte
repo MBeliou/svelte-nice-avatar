5995d4837438584a51bbd7ccccf5e2e1836ab327
Maxime Bele <maxime.bele@gmail.com>
2025-01-12 08:28:50 +0100

    hat component

---
+++ src/lib/avatar/hat/hat.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Turban from './turban.svelte';
+	import Beanie from './beanie.svelte';
+	let { style, color }: { style: 'beanie' | 'turban' | 'none'; color: string } = $props();
+</script>
+
+{#if style == 'beanie'}
+	<Beanie {color} />
+{:else if style == 'turban'}
+	<Turban {color} />
+{/if}
