4501a739d5a5181252994a92839b6dc10fdd9f9c
Maxime Bele <maxime.bele@gmail.com>
2025-02-06 14:05:24 +0100

    port nose

---
+++ src/lib/avatar/nose/nose.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Long from './long.svelte';
+	import Round from './round.svelte';
+	import Short from './short.svelte';
+
+	let { style }: { style: 'long' | 'short' | 'round' } = $props();
+</script>
+
+{#if style === 'long'}
+	<Long></Long>
+{:else if style === 'round'}
+	<Round></Round>
+{:else}
+	<Short></Short>
+{/if}
