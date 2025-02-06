949b541944ec42e840909a83df8d75c96f96465b
Maxime Bele <maxime.bele@gmail.com>
2025-02-06 14:11:42 +0100

    port shirt

---
+++ src/lib/avatar/shirt/shirt.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import chroma from 'chroma-js';
+	import Short from './short.svelte';
+	import Polo from './polo.svelte';
+	import Hoody from './hoody.svelte';
+
+	let { style, color }: { style: 'hoody' | 'polo' | 'short'; color: string } = $props();
+
+	const secondColor = chroma(color).brighten(1).hex();
+</script>
+
+{#if style === 'hoody'}
+	<Hoody {color} lightColor={secondColor}></Hoody>
+{:else if style === 'polo'}
+	<Polo {color} lightColor={secondColor}></Polo>
+{:else}
+	<Short {color}></Short>
+{/if}
