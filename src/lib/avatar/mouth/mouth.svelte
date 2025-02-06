e49628480c24f1e1fc0abe11f2eb364fdadc5615
Maxime Bele <maxime.bele@gmail.com>
2025-02-06 14:02:29 +0100

    port mouth

---
+++ src/lib/avatar/mouth/mouth.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Laugh from './laugh.svelte';
+	import Peace from './peace.svelte';
+	import Smile from './smile.svelte';
+
+	let { style }: { style: 'laugh' | 'smile' | 'peace' } = $props();
+</script>
+
+{#if style === 'laugh'}
+	<Laugh></Laugh>
+{:else if style === 'smile'}
+	<Smile></Smile>
+{:else}
+	<Peace></Peace>
+{/if}
